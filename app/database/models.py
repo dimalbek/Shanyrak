116a6d3845a4c91046655a92863bd5722d5040b6
dimalbek <dimalbek@gmail.com>
2024-04-18 12:25:10 +0500

    db created

---
+++ app/database/models.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String, Float
+from .database import Base
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String, unique=True, nullable=False)
+    phone = Column(String, unique=True, nullable=False)
+    password = Column(String, nullable=False)
+    name = Column(String, nullable=False)
+    city = Column(String, nullable=False)
