c5754ee272759c6e696312f486072a7b03cea40c
dimalbek <dimalbek@gmail.com>
2024-04-18 15:28:55 +0500

    login

--- app/api/auth/auth.py
+++ app/api/auth/auth.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, Form, Request, Response, HTTPException
 from jose import jwt
 from passlib.context import CryptContext
 from ...repositories.users import UsersRepository
-from ...serializers.users import UserCreate
+from ...serializers.users import UserCreate, UserLogin
 from ...database.database import get_db
 from sqlalchemy.orm import Session
 
@@ -43,3 +43,20 @@ def post_signup(
     return Response(
         status_code=200, content=f"successfull signup. User_id = {new_user.id}"
     )
+
+
+# login
+@router.post("/login")
+def post_login(
+    user_input: UserLogin, db: Session = Depends(get_db)
+):
+    user = users_repository.get_user_by_username(db, user_input)
+    if not verify_password(user_input.password, user.password):
+        raise HTTPException(
+            status_code=401,
+            detail="Incorrect password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    access_token = create_jwt(user.id)
+    return {"access_token": access_token, "token_type": "bearer"}
