5aadb3e47094d372fc1b3bcda0d13bf8d96d3f12
dimalbek <dimalbek@gmail.com>
2024-04-18 15:03:01 +0500

    UserCreate

---
+++ app/serializers/users.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel, EmailStr
+from pydantic_extra_types.phone_numbers import PhoneNumber
+
+
+class UserCreate(BaseModel):
+    username: EmailStr
+    phone: PhoneNumber = "+7 --- --- ----"
+    password: str
+    name: str
+    city: str
