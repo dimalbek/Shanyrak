7f93a3de0bca6f928e602fb8d346ed51c515717e
dimalbek <dimalbek@gmail.com>
2024-04-18 19:12:02 +0500

    shanyraks router added

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from app.api.auth import auth
+from app.api.shanyraks import shanyraks
 
 app = FastAPI()
 
 app.include_router(auth.router, prefix="/auth", tags=["auth"])
+app.include_router(shanyraks.router, prefix="/shanyraks", tags=["shanyraks"])
