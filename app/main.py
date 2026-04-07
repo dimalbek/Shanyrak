df3c6214145ade9ed3a52019500d1229d61feb56
dimalbek <dimalbek@gmail.com>
2026-04-07 22:29:18 +0500

    test: checky integration

--- app/main.py
+++ app/main.py
@@ -1,10 +1,8 @@
 from fastapi import FastAPI
 from app.api import auth
 from app.api import shanyraks
-from app.utils.middleware import RefreshTokenMiddleware
 
 app = FastAPI()
-app.add_middleware(RefreshTokenMiddleware)
 
 app.include_router(auth.router, prefix="/auth", tags=["auth"])
 app.include_router(shanyraks.router, prefix="/shanyraks", tags=["shanyraks"])
